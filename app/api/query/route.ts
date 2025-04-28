bd9c9fb09bd5ca0c8d2716f8c040b1602d7eb580
Moudjahid Moussa <mm12515@nyu.edu>
2025-04-28 21:23:53 +0400

    comments

--- app/api/query/route.ts
+++ app/api/query/route.ts
@@ -1,5 +1,6 @@
 import { queryPinecone } from "@/lib/pinecone/queryPinecone";
 
+// this is for querying content from the vector db. 
 export async function POST(req: Request) {
   const { query } = await req.json();
   const matches = await queryPinecone(query);
